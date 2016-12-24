033a53191418fcf25f8c935b19eff9563cb3f8b6
Criss <crisserpl2@gmail.com>
2016-12-24 02:34:32 +0100

    A few more constants (fhl::Constants)

--- EndlessRunner/src/Maths/Constants.h
+++ EndlessRunner/src/Maths/Constants.h
@@ -6,9 +6,13 @@ namespace fhl
 
 struct Constants
 {
+	constexpr static double Pi()	{ return 3.141592653589793; }
 
-	constexpr static double Pi() { return 3.141592653589793238462643383279; }
+	constexpr static double e()		{ return 2.718281828459045; }
 
+	constexpr static double sqrt2() { return 1.414213562373095; }
+
+	constexpr static double sqrt3() { return 1.732050807568877; }
 };
 
 }
