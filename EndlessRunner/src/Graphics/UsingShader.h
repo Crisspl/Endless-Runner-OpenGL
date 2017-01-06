2a14a543e8ad8a5340da742bd312d899da6fa5dc
Criss <crisserpl2@gmail.com>
2017-01-07 00:59:27 +0100

    fhl::UsingShader: set[Default|Light]Shader methods

--- EndlessRunner/src/Graphics/UsingShader.h
+++ EndlessRunner/src/Graphics/UsingShader.h
@@ -11,11 +11,16 @@ namespace fhl
 		  struct DefaultShader_t {};
 		  struct LightShader_t {};
 
+	 public:
 		  UsingShader(Shader * _default, Shader * _light) : m_defaultShader(_default), m_lightShader(_light), m_shader(_default) { }
+		  virtual ~UsingShader() = default;
 
 		  void setShader(DefaultShader_t) { m_shader = m_defaultShader; }
 		  void setShader(LightShader_t) { m_shader = m_lightShader; }
 		  void setShader(Shader & _shader) { m_shader = &_shader; }
+
+		  void setDefaultShader(Shader & _shader) { m_defaultShader = &_shader; }
+		  void setLightShader(Shader & _shader) { m_lightShader = &_shader; }
 
 	 public:
 		  static DefaultShader_t Default;
