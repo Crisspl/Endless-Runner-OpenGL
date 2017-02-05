bed602f5e7b8d36c9f2258ea1809f81c8c6f16ed
Criss <crisserpl2@gmail.com>
2017-02-05 23:54:07 +0100

    Fixed fhl::Color initializer_list ctor

--- EndlessRunner/src/Graphics/Color.cpp
+++ EndlessRunner/src/Graphics/Color.cpp
@@ -38,8 +38,13 @@ namespace fhl
 
 	 Color::Color(std::initializer_list<float> _rgba)
 	 {
-		 for (size_t i = 0; i < 4; i++)
-			 (*this)[i] = clamp(*(_rgba.begin() + i), 0.f, 1.f);
+		  for (int i = 0; i < 4; i++)
+		  {
+				if (i <= _rgba.size() - 1)
+					 (*this)[i] = clamp(*(_rgba.begin() + i), 0.f, 1.f);
+				else
+					 (*this)[i] = 1.f;
+		  }
 	 }
 
 	 bool Color::operator==(const Color & _other) const
