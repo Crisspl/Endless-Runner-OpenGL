2196641bb14fada34027e5460418d87551cfacc5
Criss <crisserpl2@gmail.com>
2017-07-06 01:07:35 +0200

    Reimplemented swizzle function

--- EndlessRunner/src/FHL/Maths/swizzle.h
+++ EndlessRunner/src/FHL/Maths/swizzle.h
@@ -5,41 +5,53 @@
 
 namespace fhl { namespace swizzle
 {
-	 enum VecDim
-	 {
-		  X = 0,
-		  Y = 1,
-		  Z = 2,
-		  W = 3
-	 };
 
-	 template<VecDim _D0, VecDim _D1, typename _VecT>
-	 constexpr Vec2<typename _VecT::valueType> get(const _VecT & _v)
-	 {
-		  static_assert(_D0 < _VecT::Dimensions && _D1 < _VecT::Dimensions,
-				"fhl::get: Not enough vector dimensions to get the one(s) demanded");
+	namespace impl
+	{
+		template<std::size_t DimsCount, typename _ValT>
+		struct VectorTypeForSize {};
 
-		  return{ _v[_D0], _v[_D1] };
-	 }
+		template<typename _ValT>
+		struct VectorTypeForSize<2, _ValT> { using Type = Vec2<_ValT>; };
 
-	 template<VecDim _D0, VecDim _D1, VecDim _D2, typename _VecT>
-	 constexpr Vec3<typename _VecT::valueType> get(const _VecT & _v)
-	 {
-		  static_assert(_D0 < _VecT::Dimensions && _D1 < _VecT::Dimensions && _D2 < _VecT::Dimensions,
-				"fhl::get: Not enough vector dimensions to get the one(s) demanded");
+		template<typename _ValT>
+		struct VectorTypeForSize<3, _ValT> { using Type = Vec3<_ValT>; };
 
-		  return Vec3<typename _VecT::valueType>(get<_D0, _D1>(_v), _v[_D2]);
-	 }
+		template<typename _ValT>
+		struct VectorTypeForSize<4, _ValT> { using Type = Vec4<_ValT>; };
 
-	 template<VecDim _D0, VecDim _D1, VecDim _D2, VecDim _D3, typename _VecT>
-	 constexpr Vec4<typename _VecT::valueType> get(const _VecT & _v)
-	 {
-		  static_assert(_D0 < _VecT::Dimensions && _D1 < _VecT::Dimensions && _D2 < _VecT::Dimensions && _D3 < _VecT::Dimensions,
-				"fhl::get: Not enough vector dimensions to get the one(s) demanded");
 
-		  return Vec4<typename _VecT::valueType>(get<_D0, _D1, _D2>(_v), _v[_D3]);
-	 }
+		template<bool...>
+		struct allTrue : std::false_type {};
 
-}} // ns
+		template<bool FirstCond, bool... Conds>
+		struct allTrue<FirstCond, Conds...> : std::integral_constant<bool, FirstCond && allTrue<Conds...>::value> {};
+
+		template<bool Cond>
+		struct allTrue<Cond> : std::integral_constant<bool, Cond> {};
+
+		template<>
+		struct allTrue<> : std::false_type {};
+	}
+
+	enum VecDim
+	{
+		X = 0, x = 0, R = 0, r = 0,
+		Y = 1, y = 1, G = 1, g = 1,
+		Z = 2, z = 2, B = 2, b = 2,
+		W = 3, w = 3, A = 3, a = 3
+	};
+
+	template<VecDim ...Dims, typename VecT>
+	constexpr typename impl::VectorTypeForSize<sizeof...(Dims), typename VecT::valueType>::Type
+	get(const VecT & _v)
+	{
+		static_assert(impl::allTrue<(Dims < VecT::Dimensions)...>::value, "Not enough vector dimensions to get the one(s) demanded");
+		static_assert(sizeof...(Dims) <= 4, "Cannot create a Vector of more than 4 dimensions");
+
+		return{ _v[Dims]... };
+	}
+
+}}
 
 #endif // FHL_SWIZZLE_H
