6e5ef6bab02d8af8071f9dee6ba76b1e041dd73d
Criss <crisserpl2@gmail.com>
2017-07-06 16:17:38 +0200

    Fixed ridiculous fhl::Constants bug

--- EndlessRunner/src/FHL/Maths/Constants.h
+++ EndlessRunner/src/FHL/Maths/Constants.h
@@ -4,18 +4,18 @@
 namespace fhl
 {
 
-	template<typename _T>
+	template<typename T>
 	struct Constants
 	{
 		Constants() = delete;
 
-		constexpr static double Pi() { return _T(3.141592653589793238); }
+		constexpr static T Pi() { return T(3.141592653589793238); }
 
-		constexpr static double e() { return _T(2.718281828459045235); }
+		constexpr static T e() { return T(2.718281828459045235); }
 
-		constexpr static double sqrt2() { return _T(1.414213562373095048); }
+		constexpr static T sqrt2() { return T(1.414213562373095048); }
 
-		constexpr static double sqrt3() { return _T(1.732050807568877293); }
+		constexpr static T sqrt3() { return T(1.732050807568877293); }
 	};
 
 }
