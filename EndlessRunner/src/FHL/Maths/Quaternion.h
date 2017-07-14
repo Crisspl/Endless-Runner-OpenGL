12b67906c919cda24cd6e4c443a8cfa161c2a2cb
Criss <crisserpl2@gmail.com>
2017-07-15 01:36:55 +0200

    Edited fhl::Quaternion a bit

--- EndlessRunner/src/FHL/Maths/Quaternion.h
+++ EndlessRunner/src/FHL/Maths/Quaternion.h
@@ -7,36 +7,36 @@
 namespace fhl
 {
 
-	 class Quaternion
-	 {
-	 public:
-		  Quaternion();
-		  Quaternion(float _x, float _y, float _z, float _angle);
-		  Quaternion(Vec3f _axis, float _angle);
+	class Quaternion
+	{
+	public:
+		constexpr Quaternion() : m_xyzw(0, 0, 0, 1) {}
+		Quaternion(float _x, float _y, float _z, float _angle);
+		Quaternion(const Vec3f & _axis, float _angle) : Quaternion(_axis.x(), _axis.y(), _axis.z(), _angle) {}
 
-		  Quaternion operator*(Quaternion _other) const;
-		  Quaternion & operator*=(const Quaternion & _other);
+		Quaternion operator*(Quaternion _other) const;
+		Quaternion & operator*=(const Quaternion & _other);
 
-		  bool operator==(const Quaternion & _other) { return m_xyzw == _other.m_xyzw; }
-		  bool operator!=(const Quaternion & _other) { return m_xyzw != _other.m_xyzw; }
+		constexpr bool operator==(const Quaternion & _other) { return m_xyzw == _other.m_xyzw; }
+		constexpr bool operator!=(const Quaternion & _other) { return m_xyzw != _other.m_xyzw; }
 
-		  Vec3f getAxis() const;
-		  float getAngle() const;
-		  Vec4f asVec4() const { return m_xyzw; }
-		  Mat4 toMat4() const;
+		Vec3f getAxis() const;
+		float getAngle() const;
+		Vec4f asVec4() const { return m_xyzw; }
+		Mat4 toMat4() const;
 
-		  float length() const;
-		  float dot(const Quaternion & _other) const;
-		  Quaternion normalized() const;
-		  Quaternion inverted() const;
-		  Quaternion conjugate() const;
+		float length() const;
+		float dot(const Quaternion & _other) const;
+		Quaternion normalized() const;
+		Quaternion inverted() const;
+		Quaternion conjugate() const;
 
-		  static Quaternion identity() { return Quaternion(); }
+		constexpr static Quaternion identity() { return Quaternion(); }
 
-	 private:
-		  Vec4f m_xyzw;
-	 };
+	private:
+		Vec4f m_xyzw;
+	};
 
-} // ns
+}
 
-#endif // FHL_QUATERNION_H
+#endif
