73a50d95bf2617d7713aceadeff3100ecb32978b
Criss <crisserpl2@gmail.com>
2017-07-22 22:07:07 +0200

    constexpr fhl::abs because std::abs is not constexpr

--- FHL/src/FHL/Maths/mathsFuncs.h
+++ FHL/src/FHL/Maths/mathsFuncs.h
@@ -11,11 +11,14 @@ namespace fhl
 	FHL_API constexpr double toRadians(double _deg) { return _deg * Constants<double>::Pi() / 180.; }
 	FHL_API constexpr double toDegrees(double _rad) { return _rad * 180. / Constants<double>::Pi(); }
 
-	template <typename _T>
-	constexpr _T clamp(_T _n, _T _lo, _T _hi)
+	template<typename T>
+	constexpr T abs(T n) { return n < T{} ? -n : n; }
+
+	template <typename T>
+	constexpr T clamp(T n, T lo, T hi)
 	{
-		return std::max(_lo, std::min(_n, _hi));
+		return std::max(lo, std::min(n, hi));
 	}
 }
 
-#endif // FHL_MATHS_FUNCS
+#endif
