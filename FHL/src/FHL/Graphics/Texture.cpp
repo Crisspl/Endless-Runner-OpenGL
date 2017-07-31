e7e6717fbb3896d7deb43bd873c22f152d3ac796
Criss <crisserpl2@gmail.com>
2017-07-31 22:37:00 +0200

    Refactored fhl::Texture

--- FHL/src/FHL/Graphics/Texture.cpp
+++ FHL/src/FHL/Graphics/Texture.cpp
@@ -1,103 +1,88 @@
 #include <FHL/Graphics/Texture.h>
 
-#include <algorithm>
+#include <utility>
 
 #include <SOIL/SOIL.h>
 
 namespace fhl
 {
 
-	 Texture::Texture(Vec2i _size)
-				 : m_size(_size),
-					m_repeated(false)
-	 {
-		 glGenTextures(1, &m_id);
-		 glBindTexture(GL_TEXTURE_2D, m_id);
+	Texture::Texture(const Vec2i & _size) : m_size(_size), m_isRepeated(false)
+	{
+		glGenTextures(1, &m_id);
 
-		 glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		 glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glBindTexture(GL_TEXTURE_2D, m_id);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _size.x(), _size.y(), 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		setRepeated(false);
 
-		 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _size.x(), _size.y(), 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
 
-		 glBindTexture(GL_TEXTURE_2D, 0);
+	Texture::Texture(const std::string & _filePath) : m_id(0u), m_size(Vec2i::zero()), m_isRepeated(false)
+	{
+		unsigned char * img = SOIL_load_image(_filePath.c_str(), &m_size.x(), &m_size.y(), 0, SOIL_LOAD_RGBA);
 
-		 setRepeated(false);
-	 }
+		if (img)
+		{
+			glGenTextures(1, &m_id);
 
-	 Texture::Texture(std::string _filePath)
-	 {
-		 uchar * tmp;
-		 std::tie(tmp, m_size, m_id) = loadImage(_filePath);
+			glBindTexture(GL_TEXTURE_2D, m_id);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_size.x(), m_size.y(), 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
+			glGenerateMipmap(GL_TEXTURE_2D);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			setRepeated(false);
 
-		 glBindTexture(GL_TEXTURE_2D, m_id);
-		 glGenerateMipmap(GL_TEXTURE_2D);
+			SOIL_free_image_data(img);
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+	}
 
-		 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	Texture::Texture(Texture && _other) :
+		m_id(_other.m_id),
+		m_size(_other.m_size),
+		m_isRepeated(_other.m_isRepeated)
+	{
+		_other.m_id = 0;
+	}
 
-		 setRepeated(false);
-	 }
+	Texture & Texture::operator=(Texture && _other)
+	{
+		std::swap(m_id, _other.m_id);
+		m_size = _other.m_size;
+		m_isRepeated = _other.m_isRepeated;
 
-	 Texture::Texture(Texture && _other) 
-			 : m_id(_other.m_id),
-				m_size(_other.m_size),
-				m_repeated(_other.m_repeated)
-	 {
-		 _other.m_id = 0;
-	 }
-
-	 Texture & Texture::operator=(Texture && _other)
-	 {
-		  std::swap(m_id, _other.m_id);
-		  m_size = _other.m_size;
-		  m_repeated = _other.m_repeated;
-
-		  return *this;
-	 }
+		return *this;
+	}
 
 
-	 Texture::~Texture()
-	 {
-		 glDeleteTextures(1, &m_id);
-	 }
+	Texture::~Texture()
+	{
+		glDeleteTextures(1, &m_id);
+	}
 
-	 const Texture& Texture::setWrapOption(Texture::WrapOption _option) const
-	 {
-		 glBindTexture(GL_TEXTURE_2D, m_id);
+	const Texture & Texture::setWrapOption(WrapOption _option) const
+	{
+		glBindTexture(GL_TEXTURE_2D, m_id);
 
-		 glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, _option);
-		 glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, _option);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, _option);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, _option);
 
-		 glBindTexture(GL_TEXTURE_2D, 0);
+		glBindTexture(GL_TEXTURE_2D, 0);
 
-		 return *this;
-	 }
+		return *this;
+	}
 
-	 std::tuple<unsigned char*, Vec2i, GLuint> Texture::loadImage(std::string _filePath)
-	 {
-		 GLuint id;
-		 Vec2i size;
-		 uchar * image = SOIL_load_image(_filePath.c_str(), &size.x(), &size.y(), 0, SOIL_LOAD_RGBA);
+	Texture & Texture::setRepeated(bool _r)
+	{
+		if (m_isRepeated == _r) return *this;
 
-		 glGenTextures(1, &id);
-		 glBindTexture(GL_TEXTURE_2D, id);
-		 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, size.x(), size.y(), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-		 SOIL_free_image_data(image);
-		 glBindTexture(GL_TEXTURE_2D, 0);
+		if (_r) setWrapOption(Repeat);
+		else setWrapOption(ClampToBorder);
+		m_isRepeated = _r;
+		return *this;
+	}
 
-		 return std::make_tuple(image, size, id);
-	 }
-
-	 Texture& Texture::setRepeated(bool _r)
-	 {
-		 if(_r)
-			 setWrapOption(Repeat);
-		 else
-			 setWrapOption(ClampToBorder);
-		 m_repeated = _r;
-		 return *this;
-	  }
-
-} // ns
+}
--- FHL/src/FHL/Graphics/Texture.h
+++ FHL/src/FHL/Graphics/Texture.h
@@ -2,63 +2,53 @@
 #define FHL_TEXTURE_H
 
 #include <string>
-#include <vector>
-#include <tuple>
 
 #include <FHL/GL/gl3w.h>
 #include <FHL/Maths/vectors.h>
 #include <FHL/common.h>
 
-namespace fhl { class ResMgr; }
-
 namespace fhl
 {
+	class ResMgr;
 
-	 class FHL_API Texture
-	 {
+	class FHL_API Texture
+	{
+		friend class ResMgr;
 
-		  friend class fhl::ResMgr;
+		template<typename, typename>
+		friend struct std::pair;
 
-		  template<typename, typename>
-		  friend struct std::pair;
+	private:
+		enum WrapOption : GLenum
+		{
+			Repeat = GL_REPEAT,
+			ClampToBorder = GL_CLAMP_TO_BORDER
+		};
 
-	 private:
-		  using uchar = unsigned char;
+	private:
+		Texture(const Texture &) = delete;
+		Texture & operator=(const Texture &) = delete;
+		Texture(Texture && _other);
+		Texture & operator=(Texture && _other);
+		explicit Texture(const Vec2i & _size);
+		explicit Texture(const std::string & _filePath);
+		~Texture();
 
-	 private:
-		  enum WrapOption
-		  {
-				Repeat = GL_REPEAT,
-				ClampToBorder = GL_CLAMP_TO_BORDER
-		  };
+		const Texture & setWrapOption(WrapOption _option) const;
 
-	 private:
-		  Texture(const Texture &) = delete;
-		  Texture & operator=(const Texture &) = delete;
-		  Texture(Texture && _other);
-		  Texture & operator=(Texture && _other);
-		  explicit Texture(Vec2i _size);
-		  explicit Texture(std::string _filePath);
-		  ~Texture();
+	public:
+		GLuint getId() const { return m_id; }
+		Vec2i getSize() const { return m_size; }
 
-		  const Texture & setWrapOption(WrapOption _option) const;
+		Texture & setRepeated(bool _r);
+		bool isRepeated() const { return m_isRepeated; }
 
-		  std::tuple<uchar *, Vec2i, GLuint> loadImage(std::string _filePath);
+	private:
+		GLuint m_id;
+		Vec2i m_size;
+		bool m_isRepeated;
+	};
 
-	 public:
-		  GLuint getId() const { return m_id; }
-		  Vec2i getSize() const { return m_size; }
+}
 
-		  Texture & setRepeated(bool _r);
-		  bool isRepeated() const { return m_repeated; }
-
-	 private:
-		  GLuint m_id;
-		  Vec2i m_size;
-		  bool m_repeated;
-	 };
-
-} // ns
-
-#endif // FHL_TEXTURE_H
-
+#endif
